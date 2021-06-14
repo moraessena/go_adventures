ba08c80f5c3feeb5881a64f222535d50089a6085
daniel sena <danielsena04@gmail.com>
2021-06-14 20:36:01 -0300

    first commit

---
+++ cmd/cyoaweb/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/danielAang/go_adventure/cyoa"
+)
+
+func main() {
+	port := flag.Int("port", 3000, "The port to start the CYOA web application on")
+	file := flag.String("file", "gopher.json", "The JSON file with the CYOA story")
+	flag.Parse()
+	fmt.Printf("Using the story in %s\n", *file)
+
+	f, err := os.Open(*file)
+	if err != nil {
+		panic(err)
+	}
+	story, err := cyoa.StoryFromJson(f)
+	if err != nil {
+		panic(err)
+	}
+	tpl := template.Must(template.New("").Parse(storyTmpl))
+
+	h := cyoa.NewHandler(story,
+		cyoa.WithTemplate(tpl),
+		cyoa.WithPathFunc(pathFn),
+	)
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
+	fmt.Println("Starting the server on port", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
+}
+
+func pathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+	return path[len("/story/"):]
+}
+
+var storyTmpl = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Choose Your Own Adventure</title>
+    </head>
+<body>
+<section class="page">
+<h1>{{.Title}}</h1>
+{{range .Paragraphs}}
+  <p>{{.}}</p>
+{{end}}
+<ul>
+{{range .Options}}
+  <li><a href="/story/{{.Chapter}}">{{.Text}}</a></li>
+{{end}}
+</ul>
+</section>
+<style>
+body {
+  font-family: helvetica, arial;
+}
+h1 {
+  text-align:center;
+  position:relative;
+}
+.page {
+  width: 80%;
+  max-width: 500px;
+  margin: auto;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  padding: 80px;
+  background: #FCF6FC;
+  border: 1px solid #eee;
+  box-shadow: 0 10px 6px -6px #797;
+}
+ul {
+  border-top: 1px dotted #ccc;
+  padding: 10px 0 0 0;
+  -webkit-padding-start: 0;
+}
+li {
+  padding-top: 10px;
+}
+a,
+a:visited {
+  text-decoration: underline;
+  color: #555;
+}
+a:active,
+a:hover {
+  color: #222;
+}
+p {
+  text-indent: 1em;
+}
+</style>
+</body>
+</html>`
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/danielAang/go_adventure
+
+go 1.16
+
+replace github.com/danielAang/go_adventure => ../go_adventure
